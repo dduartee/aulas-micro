3ae0ed553229020f8f31b191540f71ad00628f81
Gabriel Kleemann Duarte <gabrielkleemannd@gmail.com>
2026-03-06 15:57:15 -0300

    separação da atividade1 em função

--- portifolio/aula2_gpio/main.c
+++ portifolio/aula2_gpio/main.c
@@ -13,23 +13,28 @@
 #include "util/delay.h"
 
 
-// Piscar um led no pino PC2 enquanto a chave ligada ao pino PD3 estiver pressionada
+/**
+ * @brief Piscar um led no pino PC2 enquanto a chave ligada ao pino PD3 estiver pressionada
+ */
 
-
-int main(void) {
-	
+void atividade1() {
 	DDRC |= (1<<DDC2); // define o Data Direction do PC2 como saida
 	DDRD &= ~(1<<DDD3); // define o Data Direction do PD3 como saida
 	
-    while(1) {
-        // ler valor da chave
+	while(1) {
+		// ler valor da chave
 		if(PIND & (1<<PIND3)) { // filtra o PIND para comparar com PIND3, e retorna o valor do D3
 			PORTC |= (1<<PORTC2);
 			_delay_ms(300);
 			PORTC &= ~(1<<PORTC2);
 			_delay_ms(700);
-		} else {
+			} else {
 			PORTC &= ~(1<<PORTC2);
 		}
-    }
+	}
+}
+
+int main(void) {
+	
+	
 }
