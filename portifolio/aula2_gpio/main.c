ac7e4e6e52093dadee3837fb3e81dc994a81cbfc
Gabriel Kleemann Duarte <gabrielkleemannd@gmail.com>
2026-03-06 14:39:29 -0300

    adicionado projeto aula2_gpio dentro da solucao

---
+++ portifolio/aula2_gpio/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ * Created: 3/6/2026 2:36:31 PM
+ *  Author: Aluno
+ */ 
+
+#include <xc.h>
+
+// Piscar um led no pino PC2 enquanto a chave ligada ao pino PD3 estiver pressionada
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
