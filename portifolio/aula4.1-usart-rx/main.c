8dfe698410593002b4553dde46a7f9a6706d953c
Gabriel Kleemann Duarte <gabrielkleemannd@gmail.com>
2026-03-27 16:27:47 -0300

    adicionado receptor uart com led

---
+++ portifolio/aula4.1-usart-rx/main.c
@@ -0,0 +1,33 @@
+#define F_CPU 16000000
+#include <xc.h>
+
+#include "util/delay.h"
+
+void USART_initialize() {
+  UBRR0 = 103;              // 9600bps U2Xn = 0
+  UCSR0A = (0 << U2X0);     // mantem velocidade normal
+  UCSR0B = (0 << UCSZ02) |  // COMBINAÇÃO UCSZ para character size 8bits frames
+                            // de 8bits
+           (1 << RXEN0);    // habilita RX0
+
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00) |  // combinação UCSZ para character
+                                            // size 8bits frames de 8bits
+           (0 << UMSEL01) |
+           (0 << UMSEL00) |               // combinação UMSEL para USART async
+           (1 << UPM01) | (1 << UPM00) |  // habilita paridade par
+           (0 << USBS0);                  // 1 stop bit
+}
+
+int main(void) {
+  DDRB = (1 << DDB0);
+  USART_initialize();
+  while (1) {
+    while (UCSR0A & (1 << RXC0) == 0);  // espera receber alguma coisa
+    uint8_t tReceivedByte = UDR0;
+    if (tReceivedByte == 200) {
+      PORTB |= (1 << PORTB0);
+    } else {
+      PORTB = 0;
+    }
+  }
+}
