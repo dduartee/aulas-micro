5f4ef4d03e8421609285d0b2be4e17a24c9f121e
Gabriel Kleemann Duarte <gabrielkleemannd@gmail.com>
2026-03-29 11:38:05 -0300

    receptor recebe estado das chaves do transmissor com verificação de checksum

--- portifolio/aula4.1-usart-rx/main.c
+++ portifolio/aula4.1-usart-rx/main.c
@@ -3,6 +3,8 @@
 
 #include "util/delay.h"
 
+uint8_t gReceivedByte[3];  // 2 dados + 1 checksum
+
 void USART_initialize() {
   UBRR0 = 103;              // 9600bps U2Xn = 0
   UCSR0A = (0 << U2X0);     // mantem velocidade normal
@@ -17,17 +19,55 @@ void USART_initialize() {
            (1 << UPM01) | (1 << UPM00) |  // habilita paridade par
            (0 << USBS0);                  // 1 stop bit
 }
+void USART_receiveMessage(uint8_t* pBuffer, int pSize) {
+  uint8_t* tBufferPtr = pBuffer;
+  for (int i = 0; i < pSize; i++) {
+    while ((UCSR0A & (1 << RXC0)) == 0);  // espera receber alguma coisa
+    uint8_t tReceivedByte = UDR0;
+    *tBufferPtr = tReceivedByte;
+    tBufferPtr++;
+  }
+}
+
+void USART_parseMessage(uint8_t* pMessage) {
+  uint8_t tChecksum = 0;
+  for (int i = 0; i < 2; i++) {
+    tChecksum += pMessage[i]; // calcula o checksum dos dados
+  }
+  if (pMessage[2] == tChecksum) { // valida
+    PORTB &= ~(1 << PORTB2);
+    switch (pMessage[0]) {
+      case 0x20: // comando para o led B0
+        if (pMessage[1] == 0x01) {  
+          PORTB |= (1 << PORTB0);
+        } else {
+          PORTB &= ~(1 << PORTB0);
+        }
+        _delay_ms(10);
+
+        break;
+      case 0x21: // comando para o led B1
+        if (pMessage[1] == 0x01) {  
+          PORTB |= (1 << PORTB1);
+        } else {
+          PORTB &= ~(1 << PORTB1);
+        }
+        _delay_ms(10);
+        break;
+      default:
+        break;
+    }
+  } else {
+    PORTB |= (1 << PORTB2); // acusa erro de checksum
+  }
+}
 
 int main(void) {
-  DDRB = (1 << DDB0);
+  DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2);
   USART_initialize();
   while (1) {
-    while (UCSR0A & (1 << RXC0) == 0);  // espera receber alguma coisa
-    uint8_t tReceivedByte = UDR0;
-    if (tReceivedByte == 200) {
-      PORTB |= (1 << PORTB0);
-    } else {
-      PORTB = 0;
-    }
+    USART_receiveMessage(gReceivedByte, 3);
+    USART_parseMessage(gReceivedByte);
+    _delay_ms(1);
   }
 }
--- portifolio/aula4.1-usart-tx/main.c
+++ portifolio/aula4.1-usart-tx/main.c
@@ -3,11 +3,7 @@
 
 #include "util/delay.h"
 
-#define BUFFER_SIZE 3
-// uint8_t gMessage[BUFFER_SIZE] = {200, 100, 50, 94};  // ultimo byte eh
-// checksum (soma todos
-//  e modulo (resto) por 256) 350mod256
-uint8_t gMessage[BUFFER_SIZE] = {200, 100, 50};
+
 
 void USART_initialize() {
   UBRR0 = 103;              // 9600bps U2Xn = 0
@@ -24,24 +20,49 @@ void USART_initialize() {
            (0 << USBS0);                  // 1 stop bit
 }
 
+/**
+ * transmite a mensagem {0x20, 0x0n, 0xCS} dados com checksum
+ */
 void UART_sendMessage(uint8_t* pData, int pSize) {
   uint8_t* tMsgPtr = pData;  // aponta para o endereço da mensagem
   uint8_t tChecksum = 0;
   for (int i = 0; i < pSize; i++) {
     while ((UCSR0A & (1 << UDRE0)) == 0);  // espera a liberação
     uint8_t tMessageByte = *tMsgPtr;
-    tChecksum += tMessageByte;  // soma os bytes (quando estoura o tamanho, reinicia a contagem, por isso calcula manualmente o "mod")
-    UDR0 = tMessageByte;        // transmite a mensagem
-    tMessageByte++;             // move o ponteiro
+    tChecksum +=
+        tMessageByte;     // soma os bytes (quando estoura o tamanho, reinicia a
+                          // contagem, por isso calcula manualmente o "mod")
+    UDR0 = tMessageByte;  // transmite a mensagem
+    tMsgPtr++;            // move o ponteiro
   }
   while ((UCSR0A & (1 << UDRE0)) == 0);  // espera a liberação
+  // UDR0 = tChecksum++; // para falhar o checksum
   UDR0 = tChecksum;
 }
 
+// ultimo estado dos botões b0 e b1
+uint8_t gStateB0 = 0x00;
+uint8_t gStateB1 = 0x00;
+/**
+ * objetivo: enviar o estado atual dos botões a cada mudança
+ */
 int main(void) {
   USART_initialize();
   while (1) {
-    UART_sendMessage(gMessage, BUFFER_SIZE);
-    _delay_ms(10);
+    uint8_t tStateB0 = (PINB & (1 << PINB0)) == 0 ? 0x01 : 0x00;
+    if (tStateB0 != gStateB0) { // verifica estado atual x anterior
+      uint8_t tMessageB0[2] = {0x20, tStateB0};
+      UART_sendMessage(tMessageB0, 2);
+      _delay_ms(10);
+      gStateB0 = tStateB0; // substitui
+    }
+
+    uint8_t tStateB1 = (PINB & (1 << PINB1)) == 0 ? 0x01 : 0x00;
+    if (tStateB1 != gStateB1) {
+      uint8_t tMessageB1[2] = {0x21, tStateB1};
+      UART_sendMessage(tMessageB1, 2);
+      _delay_ms(10);
+      gStateB1 = tStateB1;
+    }
   }
 }
