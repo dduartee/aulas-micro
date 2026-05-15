7cc67d1e951eefd0cd1a4de57c9426e96ab57d35
Gabriel Kleemann Duarte <gabrielkleemannd@gmail.com>
2026-05-15 16:04:04 -0300

    usando o timer 0A e 0B para geração de pwm com 60%

--- portifolio/aula7-timer/main.c
+++ portifolio/aula7-timer/main.c
@@ -3,17 +3,21 @@
 #include <xc.h>
 #include "avr/interrupt.h"
 
-ISR(TIMER0_OVF_vect) {
-    PORTB ^= (1<<PORTB0); // alterna porta, duas interrupções para gerar a onda
+ISR(TIMER0_COMPA_vect) {
+    PORTB |= (1<<PORTB0); // eleva PB0
+}
+
+ISR(TIMER0_COMPB_vect) {
+    PORTB &= ~(1<<PORTB0); // baixa PB0
 }
 
 int main(void) {
     DDRB = (1<<DDB0);
     TCCR0A = (1<<WGM01) | (0<<WGM00); // WGM00 WGM01 WGM02 = modo CTC conta até OCR0A
     TCCR0B = (0<<WGM02) | (0<<CS02) | (1<<CS01) | (0<<CS00); // prescaler de /8
-    OCR0A = 249; // contagem até 250 uS (para F_clk @ 1MHz)
-
-    TIMSK0 = (1<<TOIE0); // interrupção de overflow
+    OCR0A = 249; // contagem até 250 us (para F_clk @ 1MHz)
+    OCR0B = 99; // 100us transição da onda com duty cycle 40%
+    TIMSK0 = (1<<OCIE0A) | (1<<OCIE0B); // interrupções A e B de comparação de saída
     sei();
     
     while(1) {}
