99f7b0b8a3318154c144d605d0cd59e1e47cd8dc
Gabriel Kleemann Duarte <gabrielkleemannd@gmail.com>
2026-05-15 16:45:13 -0300

    usando gerador de pwm com dois timers sem interrupção

--- portifolio/aula7.1-timer-match/main.c
+++ portifolio/aula7.1-timer-match/main.c
@@ -1,15 +1,23 @@
 #define F_CPU 8000000
 #include "avr/io.h"
 #include <xc.h>
+#include "util/delay.h"
+
+uint8_t gPWM = 0;
 
 int main(void) {
-    DDRB = (1<<DDB6);
-    DDRD = (1<<DDD6);
-    
-    TCCR0A = (1<<WGM01) | (0<<WGM00) | (1<<COM0A0) | (0<<COM0A1); // modo ctc, toggle do OC0A
-    TCCR0B = (0<<WGM02) | (0<<CS02) | (1<<CS01) | (0<<CS00); // prescaler de /8
-    OCR0A = 249; // contagem até 250 us (para F_clk @ 1MHz)
-    //OCR0B = 99; // 100us transição da onda com duty cycle 40%
-    
-    while(1) {}
+  DDRD = (1 << DDD6) | (1<<DDD5);
+
+  TCCR0A = (1 << WGM01) | (1 << WGM00) // modo FAST PWM com TOP=0xFF, OC0A PWM não-inversor
+         | (0 << COM0B0) | (1 << COM0B1)
+         | (0 << COM0A0) | (1 << COM0A1); 
+  
+  TCCR0B = (1 << WGM02) | (0 << CS02) | (1 << CS01) | (0 << CS00); // prescaler de /8
+  OCR0A = 99;
+  while (1) {
+      gPWM++;
+      if(gPWM>99) gPWM = 0;
+      OCR0B = gPWM;
+      _delay_ms(100);
+  }
 }
